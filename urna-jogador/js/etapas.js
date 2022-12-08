df11f4e43be24118013276bb113821af1ff0863a
Isabela Moraes <95056854+isabelagueiros@users.noreply.github.com>
2022-12-08 17:26:19 -0300

    Add files via upload

---
+++ urna-jogador/js/etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+  {
+      titulo: 'GOLEIRO',
+      numeros: 3,
+      candidatos: [
+          {
+              numero: '999',
+              nome: 'Weverton',
+              pais: 'Brail',
+              fotos:[
+                  {url:'weverton.png', posicao: 'Defesa'}
+              ]
+          },
+          {
+              numero: '222',
+              nome: 'Ochoa',
+              pais: 'México',
+              fotos:[
+                  {url:'ochoa.png', posicao: 'Defesa'}
+              ]
+          },
+          {
+            numero: '444',
+            nome: 'Hugo Lloris ',
+            pais: 'França',
+            fotos:[
+                {url:'iloris.frança.png', posicao: 'Defesa'}
+            ]
+        },
+      ]
+  },
+  {
+      titulo: 'JOGADOR',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '11',
+              nome: 'Angelo Di Maria',
+              pais: 'Argentina',
+              fotos:[
+                  {url:'angelo-di-maria.png', posicao: 'Meia'},
+              ]
+          },
+          {
+              numero: '10',
+              nome: 'Memphis Depay',
+              pais: 'Paises Baixos',
+              fotos:[
+                  {url:'memphis-depay-paisesbaixos.png', posicao: 'Atacante'},
+              ]
+          },
+          {
+            numero: '09',
+            nome: 'Richarlyson',
+            pais: 'Brasil',
+            fotos:[
+                {url:'richarlyson-brasil.png', posicao: 'Atacante'},
+            ]
+        },
+        {
+            numero: '22',
+            nome: 'Jude Bellingham',
+            pais: 'Inglaterra',
+            fotos:[
+                {url:'jude-bellingham-inglaterra.png', posicao: 'Meia'},
+            ]
+        },
+      ]
+  }
+];
